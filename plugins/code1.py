2f6105843b3ad8e32826c9ce3a00f33be9572e8c
涵曦 <im.hanxi@gmail.com>
2024-09-10 16:24:11 +0000

    fix: 修复插件示例报错 #105

--- plugins/code1.py
+++ plugins/code1.py
@@ -1,4 +1,5 @@
 async def code1(arg1):
     global log, xiaomusic
     log.info(f"code1:{arg1}")
-    await xiaomusic.do_tts("你好，我是自定义的测试口令")
+    did = xiaomusic._cur_did
+    await xiaomusic.do_tts(did, "你好，我是自定义的测试口令")
