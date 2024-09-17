09545c701506a68e4d4657cfb37faf4d1718cb4f
52fisher <32198215+52fisher@users.noreply.github.com>
2024-09-18 06:48:38 +0800

    fix: 修复 Pure 主题白屏无法打开的问题 (#176)
    * 修复 白屏无法打开的问题
    * 增加清空已缓存的设置和拉取最新设置功能
    * 增加刷新页面按钮及说明

---
+++ xiaomusic/static/pure/assets/AboutView-D8QMKsiA.js
@@ -0,0 +1,1 @@
+import{_ as o,c as s,a as t,o as a}from"./index-DHfKp1Ci.js";const n={},c={class:"about"};function r(_,e){return a(),s("div",c,e[0]||(e[0]=[t("h1",null,"This is an about page",-1)]))}const l=o(n,[["render",r]]);export{l as default};
