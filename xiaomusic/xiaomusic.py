43342979b4f7b81553ff95376a50c90c3cc77694
hui <760261797@qq.com>
2024-09-18 19:32:43 +0800

    fix:修复开启继续播放时歌曲播放不完整问题

--- xiaomusic/xiaomusic.py
+++ xiaomusic/xiaomusic.py
@@ -536,6 +536,11 @@ class XiaoMusic:
                 query = new_record.get("query", "").strip()
                 did = new_record.get("did", "").strip()
                 await self.do_check_cmd(did, query, False)
+                answers = new_record.get("answers", [{}])
+                if answers:
+                    answer = answers[0].get("tts", {}).get("text", "").strip()
+                    await self.reset_timer_when_answer(len(answer), did)
+                    self.log.debug(f"query:{query} did:{did} answer:{answer}")
 
     # 匹配命令
     async def do_check_cmd(self, did="", query="", ctrl_panel=True, **kwargs):
@@ -551,6 +556,10 @@ class XiaoMusic:
             await func(did=did, arg1=oparg)
         except Exception as e:
             self.log.exception(f"Execption {e}")
+
+    # 重置计时器
+    async def reset_timer_when_answer(self, answer_length, did):
+        await self.devices[did].reset_timer_when_answer(answer_length)
 
     def append_running_task(self, task):
         self.running_task.append(task)
@@ -938,6 +947,7 @@ class XiaoMusicDevice:
         # 播放进度
         self._start_time = 0
         self._duration = 0
+        self._paused_time = 0
 
         # 关机定时器
         self._stop_timer = None
@@ -948,9 +958,9 @@ class XiaoMusicDevice:
         return self.device.cur_music
 
     def get_offset_duration(self):
-        if not self._playing:
+        if not self.isplaying():
             return -1, -1
-        offset = time.time() - self._start_time
+        offset = time.time() - self._start_time - self._paused_time
         duration = self._duration
         return offset, duration
 
@@ -1084,6 +1094,7 @@ class XiaoMusicDevice:
         sec = sec + self.config.delay_sec
         self._start_time = time.time()
         self._duration = sec
+        self._paused_time = 0
         await self.set_next_music_timeout(sec)
         self.xiaomusic.save_cur_config()
 
@@ -1334,6 +1345,19 @@ class XiaoMusicDevice:
         except Exception as e:
             self.log.error(f"_get_audio_id {e}")
         return str(audio_id)
+
+    # 重置计时器
+    async def reset_timer_when_answer(self, answer_length):
+        if not (self.isplaying() and self.config.continue_play):
+            return
+        pause_time = answer_length / 5 + 1
+        offset, duration = self.get_offset_duration()
+        self._paused_time += pause_time
+        new_time = duration - offset + pause_time
+        await self.set_next_music_timeout(new_time)
+        self.log.info(
+            f"reset_timer 延长定时器. answer_length:{answer_length} pause_time:{pause_time}"
+        )
 
     # 设置下一首歌曲的播放定时器
     async def set_next_music_timeout(self, sec):
