8114d196f443bfdb9473647fabb3d7380dab60a7
clarencejh <jhcj.z@qq.com>
2025-02-08 18:49:11 +0800

    Merge branch 'main' of github.com:xiaomusic

--- xiaomusic/httpserver.py
+++ xiaomusic/httpserver.py
@@ -8,7 +8,7 @@ import tempfile
 import urllib.parse
 from contextlib import asynccontextmanager
 from dataclasses import asdict
-from typing import Annotated, TYPE_CHECKING
+from typing import TYPE_CHECKING, Annotated
 
 if TYPE_CHECKING:
     from xiaomusic.xiaomusic import XiaoMusic
