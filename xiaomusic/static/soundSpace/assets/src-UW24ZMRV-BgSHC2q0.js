f887b07d1c669cde8901d7bb4e7c2a7ebc211ebe
jhao <jhao413@qq.com>
2025-11-02 20:16:12 +0800

    feat: add path aliases and base URL configuration in Vite（SoundSpace）

--- xiaomusic/static/soundSpace/assets/src-UW24ZMRV-BgSHC2q0.js
+++
@@ -1,1 +0,0 @@
-import{O as a}from"./features-animation-d7XZ5Dr3.js";import"./index-BlfQbszy.js";var t=a;export{t as default};
---
+++ xiaomusic/static/soundSpace/assets/src-UW24ZMRV-VnAVJcYj.js
@@ -0,0 +1,1 @@
+import{O as a}from"./features-animation-BRiA9rfd.js";import"./index-ihgeXEwv.js";var t=a;export{t as default};
