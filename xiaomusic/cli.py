46242a993322b8c302166ddda5f1198c36ce1c49
涵曦 <im.hanxi@gmail.com>
2026-01-15 22:21:26 +0800

    fix: 修复启动报错

--- xiaomusic/cli.py
+++ xiaomusic/cli.py
@@ -217,8 +217,7 @@ def main():
         # 运行服务器
         await server.serve()
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(async_main(config))
+    asyncio.run(async_main(config))
 
 
 if __name__ == "__main__":
