33d7bb751fd269d9e2aa2f7584b87ad6fe4d2b1a
fragrans <fa545516381@gmail.com>
2026-02-11 17:11:22 +0800

    Update device.py
    增加获取设备列表

--- xiaomusic/api/routers/device.py
+++ xiaomusic/api/routers/device.py
@@ -21,6 +21,12 @@ from xiaomusic.api.models import (
 
 router = APIRouter(dependencies=[Depends(verification)])
 
+@router.get("/device_list")
+async def device_list():
+    """获取设备列表"""
+    await xiaomusic.auth_manager.init_all_data()
+    devices = await xiaomusic.auth_manager.try_update_device_id()
+    return {"devices": devices}
 
 @router.get("/getvolume")
 async def getvolume(did: str = ""):
